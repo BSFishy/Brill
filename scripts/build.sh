3b44ba2ee695414aece472a1ddb671f6db8d0d7c
Matt <mttprvstanddad@gmail.com>
2019-09-17 19:52:10 -0500

    Get cmake working

---
+++ scripts/build.sh
@@ -0,0 +1,1 @@
+cmake --build "cmake-build-debug" --target brill -- -j 4
--- scripts/create.sh
+++ scripts/create.sh
@@ -1,7 +1,7 @@
-if [ ! -d "cmake-build" ]; then
-  mkdir cmake-build
+if [ ! -d "cmake-build-debug" ]; then
+  mkdir cmake-build-debug
 fi
 
-pushd cmake-build
+pushd cmake-build-debug
 cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=/usr/bin/clang-9 -DCMAKE_CXX_COMPILER=/usr/bin/clang++-9 -G "CodeBlocks - Unix Makefiles" ../
 popd
