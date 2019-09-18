95ee828ae1b8e76638239f9edf2c24ffa72de3a4
BSFishy <mttprvstanddad@gmail.com>
2019-09-18 00:46:27 +0000

    Changes

--- scripts/create.sh
+++ scripts/create.sh
@@ -1,1 +1,7 @@
-cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=/usr/bin/clang-9 -DCMAKE_CXX_COMPILER=/usr/bin/clang++-9 -G "CodeBlocks - Unix Makefiles" .
+if [ ! -d "cmake-build" ]; then
+  mkdir cmake-build
+fi
+
+pushd cmake-build
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=/usr/bin/clang-9 -DCMAKE_CXX_COMPILER=/usr/bin/clang++-9 -G "CodeBlocks - Unix Makefiles" ../
+popd
