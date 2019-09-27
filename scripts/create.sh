1996e1705bea77004ad95d0a56c0ed2cc2fca040
BSFishy <mttprvstanddad@gmail.com>
2019-09-27 17:34:58 +0000

    Fix clang version

--- scripts/create.sh
+++ scripts/create.sh
@@ -3,5 +3,5 @@ if [ ! -d "cmake-build-debug" ]; then
 fi
 
 pushd cmake-build-debug
-cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=/usr/bin/clang-9 -DCMAKE_CXX_COMPILER=/usr/bin/clang++-9 -G "CodeBlocks - Unix Makefiles" ../
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=/usr/bin/clang-10 -DCMAKE_CXX_COMPILER=/usr/bin/clang++-10 -G "CodeBlocks - Unix Makefiles" ../
 popd
