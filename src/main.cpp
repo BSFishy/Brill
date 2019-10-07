ed60c679d390d5647d5c6d90ed56aa5b3266fd74
BSFishy <mttprvstanddad@gmail.com>
2019-10-07 17:34:31 +0000

    Add file output

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,15 @@
 #include <iostream>
 #include <memory>
 
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/Support/FileSystem.h"
+#include "llvm/Support/Host.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/Support/TargetRegistry.h"
+#include "llvm/Support/TargetSelect.h"
+#include "llvm/Target/TargetMachine.h"
+#include "llvm/Target/TargetOptions.h"
+
 #include "brill.h"
 
 using namespace std;
@@ -12,7 +21,7 @@ int main(int argc, const char *argv[]) {
         return 0;
     }
 
-    fprintf(stderr, "Compiling: %s\n", argv[1]);
+    fprintf(stderr, "Compiling: %s\n\n", argv[1]);
 
     std::shared_ptr<Brill::ParseContext> file = Brill::parseFile(argv[1]);
     Brill::convert(file->topLevel);
@@ -20,7 +29,58 @@ int main(int argc, const char *argv[]) {
     std::shared_ptr<Brill::IR::Module> module = Brill::getOrCreateModule("test");
     std::shared_ptr<Brill::IR::CodegenContext> ctx = module->codegen();
 
+    fprintf(stderr, "Output:\n\n");
     ctx->module->print(llvm::errs(), nullptr);
+
+    // EMIT FILE STUFF
+
+    llvm::InitializeAllTargetInfos();
+    llvm::InitializeAllTargets();
+    llvm::InitializeAllTargetMCs();
+    llvm::InitializeAllAsmParsers();
+    llvm::InitializeAllAsmPrinters();
+
+    auto targetTriple = llvm::sys::getDefaultTargetTriple();
+    ctx->module->setTargetTriple(targetTriple);
+
+    std::string error;
+    auto target = llvm::TargetRegistry::lookupTarget(targetTriple, error);
+
+    if (!target) {
+        fprintf(stderr, "%s\n", error.c_str());
+        return 1;
+    }
+
+    auto cpu = "generic";
+    auto features = "";
+
+    llvm::TargetOptions opt;
+    auto rm = llvm::Optional<llvm::Reloc::Model>();
+    auto targetMachine = target->createTargetMachine(targetTriple, cpu, features, opt, rm);
+
+    ctx->module->setDataLayout(targetMachine->createDataLayout());
+
+    auto filename = "output";
+    std::error_code ec;
+    llvm::raw_fd_ostream dest(filename, ec, llvm::sys::fs::OF_None);
+
+    if (ec) {
+        fprintf(stderr, "Could not open file: %s\n", ec.message().c_str());
+        return 1;
+    }
+
+    llvm::legacy::PassManager pass;
+    auto fileType = llvm::TargetMachine::CGFT_AssemblyFile;
+
+    if (targetMachine->addPassesToEmitFile(pass, dest, nullptr, fileType)) {
+        fprintf(stderr, "Could not emit file of type\n");
+        return 1;
+    }
+
+    pass.run(*(ctx->module));
+    dest.flush();
+
+    fprintf(stderr, "Wrote file %s\n", filename);
 
     return 0;
 }
